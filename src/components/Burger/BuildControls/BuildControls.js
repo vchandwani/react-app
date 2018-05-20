e3749bf448c602afe4821d89f5358843fab0943d
varunchandwani <varunchandwani@gmail.com>
2018-05-20 18:41:46 +1000

    Burger Remove Ingredients
    Burger Remove Ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,11 +11,14 @@ const controls = [
 const buildControls = (props) => {
     return(
     <div className={classes.BuildControls}> 
+        <p>Current Price : <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map( ctrl => (
             <BuildControl 
                 key={ctrl.label} 
                 label={ctrl.label}
                 added={() => props.ingredientsAdded(ctrl.type)} 
+                removed={() => props.ingredientsRemoved(ctrl.type)} 
+                disabled = {props.disabled[ctrl.type]}
             />
         )
         )}
