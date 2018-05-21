4e09e160079c39af4b25efda68db7bbd57b633d6
varunchandwani <varunchandwani@gmail.com>
2018-05-21 14:03:58 +1000

    AUX file rename
    AUX file rename

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/ReactAux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 
