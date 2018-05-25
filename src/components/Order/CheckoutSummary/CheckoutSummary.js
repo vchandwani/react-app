15dbc4bce31dceb82f7ed3eab3c249134f33ce06
varunchandwani <varunchandwani@gmail.com>
2018-05-25 13:30:41 +1000

    Navigation
    Navigation

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const CheckoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes good</h1>
+            <div style={{width:'100%',height:'300px',margin:'auto'}}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button 
+                btnType="Danger">
+                Cancel</Button>
+            <Button 
+                btnType="Success">
+                Continue</Button>
+        </div>
+    );
+}
+export default CheckoutSummary;
