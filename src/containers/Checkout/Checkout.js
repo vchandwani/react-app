9f33a06bdec72c2380d7a8301e3b76e28a36e368
varunchandwani <varunchandwani@gmail.com>
2018-05-25 14:51:32 +1000

    Contact details
    Contact details

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -32,6 +34,7 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelHandler}
                     checkoutContinued={this.checkoutContinueHandler}
                 />
+                <Route path={this.props.match.path+'/contact-data'} component={ContactData}>Contact Data</Route>
             </div>
         )
     }
