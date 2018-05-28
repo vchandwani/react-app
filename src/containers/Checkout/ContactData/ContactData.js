10e67b49cb655cc4a17b6dbd5419327d53da0d4c
varunchandwani <varunchandwani@gmail.com>
2018-05-28 10:18:45 +1000

    Form Validation
    Form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,9 +17,10 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required :true
+                    required: true
                 },
-                valid : false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -29,9 +30,10 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required :true
+                    required: true
                 },
-                valid : false
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -41,11 +43,13 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required :true,
-                    minLength : 5,
-                    maxLength : 5
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                    isNumeric: true
                 },
-                valid : false
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -55,9 +59,10 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required :true
+                    required: true
                 },
-                valid : false
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -67,9 +72,11 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required :true
+                    required: true,
+                    isEmail: true
                 },
-                valid : false
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -79,9 +86,12 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                value: '',
+                validation: {},
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -107,18 +117,34 @@ class ContactData extends Component {
             } );
     }
 
-    checkValidity(value, rules){
-        let isValid = false;
-
-        if(rules.required){
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
-        if(rules.minLength){
-            isValid = value.length >= rules.minLength && isValid;
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
-        if(rules.maxLength){
-            isValid = value.length <= rules.maxLength && isValid;
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
         }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
 
@@ -130,9 +156,15 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value,updatedFormElement.validation);
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        this.setState({orderForm: updatedOrderForm});
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render () {
@@ -151,9 +183,12 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success">ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if ( this.state.loading ) {
