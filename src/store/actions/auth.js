51bc665c1cfcf7f82102c68917cfb3412c7789d9
vchandwani <varunchandwani@gmail.com>
2020-01-29 14:20:20 +1100

    Token added to Orders Api
    Token added to Orders Api

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,18 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+      type: actionTypes.AUTH_LOGOUT
+    }
+}
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(()=>{
+      dispatch(logout());
+    },expirationTime*1000);
+  }
+}
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
