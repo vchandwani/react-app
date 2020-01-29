2616502886a3131f824d3667626d0615d2bfd8e1
vchandwani <varunchandwani@gmail.com>
2020-01-29 14:37:02 +1100

    Logout added and redirected after logout clicked
    Logout added and redirected after logout clicked

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../ReactAux/ReactAux';
 import classes from './Layout.css';
@@ -23,8 +24,9 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <Toolbar isAuth={this.props.isAuthenticated} drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
+                    isAuth={this.props.isAuthenticated} 
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
@@ -34,5 +36,10 @@ class Layout extends Component {
         )
     }
 }
+const mapStateToProps = state =>{
+    return {
+        isAuthenticated : state.auth.token !== null
+    }
+}
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
