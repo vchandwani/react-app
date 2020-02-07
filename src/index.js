a4c368556c8f43e385f1d1184ddd4588d1726726
vchandwani <varunchandwani@gmail.com>
2020-02-07 15:28:05 +1100

    Changes for Index file
    Changes for Index file

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 import { watchAuth , watchBurgerBuilder, watchOrder } from './store/sagas/index';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
