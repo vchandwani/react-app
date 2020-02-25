4b196634fe9a0b68cbf8f4d15a1e075c9c08912e
vchandwani <varunchandwani@gmail.com>
2020-02-25 16:07:46 +1100

    Integration Test file
    Integration Test file

---
+++ src/integration.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {storeFactory } from '../test/testUtils';
+import {guessWord} from './store/actions/index';
+
+
+describe('burgerBuilder action dispatcher', ()=> {
+
+})
